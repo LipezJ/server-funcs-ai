93ea397351430a99908570abe57a28e84a6a01c6
Juan Lipez <juandavidlipez47@gmail.com>
2024-08-06 10:52:23 -0500

    add @astrojs/vercel package for serverless deployment

--- frontend/astro.config.ts
+++ frontend/astro.config.ts
@@ -2,17 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import auth from "auth-astro";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../backend/ui',
   output: "server",
   integrations: [tailwind(), react(), auth()],
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   experimental: {
     serverIslands: true
   }
