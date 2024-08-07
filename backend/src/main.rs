bccf49c7509fe1dd35bb34c66fa83487f3dd33ca
Juan Lipez <89532423+LipezJ@users.noreply.github.com>
2024-08-07 16:41:06 -0500

    update server to usability

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -47,7 +47,7 @@ async fn main() {
 }
 
 async fn serve(app: Router, port: u16) {
-	let addr = SocketAddr::from(([127, 0, 0, 1], port));
+	let addr = SocketAddr::from(([0, 0, 0, 0], port));
 	let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 	axum::serve(listener, app).await.unwrap();
 }
